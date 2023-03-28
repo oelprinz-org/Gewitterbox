b19e81bc52f1efd36d360e9ebe3cd7b94ba3d7fa
chaoschris <42533712+chaoschris@users.noreply.github.com>
2023-03-28 09:34:18 +0200

    Add files via upload

---
+++ software/202207.1/speeduino-202207.1/speeduino/init.h
@@ -0,0 +1,10 @@
+#ifndef INIT_H
+#define INIT_H
+
+void initialiseAll();
+void initialiseTriggers();
+void setPinMapping(byte);
+void changeHalfToFullSync(void);
+void changeFullToHalfSync(void);
+
+#endif
